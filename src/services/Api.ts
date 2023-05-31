e7cb6f6b8ac89b92200f7d40bd49105825fb296d
brasileiroapps <100478178+brasileiroapps@users.noreply.github.com>
2023-05-31 19:08:41 -0300

    update code

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-     baseURL:"http://45.178.182.24:3333",
+     baseURL:"http://45.178.182.24:3333/",
     //baseURL:"http://192.168.3.4:3333/",
 })
 
